87de25bfcd8de39916b83284ed5e224b23e5b5e4
Ahmad Raza <ahmadraza12ab@gmail.com>
2023-04-26 15:52:10 +0530

    videos and channel

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-const Sidebar = () => {
-  const selectedCategory = "New";
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
   return (
     <Stack
       direction="row"
@@ -17,6 +16,7 @@ const Sidebar = () => {
         return (
           <button
             className="category-btn"
+            onClick={() => setSelectedCategory(category.name)}
             style={{
               background: category.name === selectedCategory && "#FC1503",
               color: "white",
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
-const BASE_URL = "https://youtube-v38.p.rapidapi.com/search/";
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 const options = {
+  url: BASE_URL,
   params: {
-    maxResults: "50",
+    part: "snippet",
+    videoId: "M7FIvfx5J10",
   },
   headers: {
     "content-type": "application/octet-stream",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    "X-RapidAPI-Host": "youtube-v38.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_APP_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
-export const fetchFromAPI = async(url)=>{
-    const {data}=await axios.get(`${BASE_URL}/${url}`,options)
-    return data;
-}
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
