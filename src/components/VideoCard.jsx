c4bcb0f930ac33043ca26ebb5b6bf2ec39ca681e
Ahmad Raza <ahmadraza12ab@gmail.com>
2023-04-28 10:14:31 +0530

    finish project

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -14,7 +14,7 @@ const VideoCard = ({ video: { id: videoId }, snippet }) => {
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100%" },
+        width: { xs: "100%", md: "320px", sm: "358px" },
         boxShadow: "node",
         borderRadius: 0,
       }}
@@ -23,7 +23,14 @@ const VideoCard = ({ video: { id: videoId }, snippet }) => {
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{
+            width: {
+              xs: "100%",
+              sm: "358px",
+              md: "320px",
+            },
+            height: 180,
+          }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
